cc1d3e6cb9e8a50511b96f45ce8a5a334eb530d4
shahdabdo335 <shahd.abdelrahman.eelu@gmail.com>
2026-09-24 23:29:28 +0300

    Add project files.

---
+++ ConsoleApp13/Data/ApplicationDbContext.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApp13.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp13.Data;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderItem> OrderItems { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Staff> Staffs { get; set; }
+
+    public virtual DbSet<Stock> Stocks { get; set; }
+
+    public virtual DbSet<Store> Stores { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=.;initial catalog= BIKESTORE ;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(e => e.BrandId).HasName("PK__brands__5E5A8E274AD5F167");
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__categori__D54EE9B4913DB24D");
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PK__customer__CD65CB858EC4F5BF");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__orders__46596229BE941E17");
+
+            entity.ToTable("orders", "sales", tb => tb.HasTrigger("AFTER_INSERTED"));
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Orders)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__orders__customer__20C1E124");
+
+            entity.HasOne(d => d.Staff).WithMany(p => p.Orders)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__orders__staff_id__22AA2996");
+
+            entity.HasOne(d => d.Store).WithMany(p => p.Orders).HasConstraintName("FK__orders__store_id__21B6055D");
+        });
+
+        modelBuilder.Entity<OrderItem>(entity =>
+        {
+            entity.HasKey(e => new { e.OrderId, e.ItemId }).HasName("PK__order_it__837942D453621804");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderItems).HasConstraintName("FK__order_ite__order__267ABA7A");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderItems).HasConstraintName("FK__order_ite__produ__276EDEB3");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__products__47027DF5A08195C4");
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Products).HasConstraintName("FK__products__brand___15502E78");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products).HasConstraintName("FK__products__catego__145C0A3F");
+        });
+
+        modelBuilder.Entity<Staff>(entity =>
+        {
+            entity.HasKey(e => e.StaffId).HasName("PK__staffs__1963DD9C2A615B51");
+
+            entity.HasOne(d => d.Manager).WithMany(p => p.InverseManager).HasConstraintName("FK__staffs__manager___1DE57479");
+
+            entity.HasOne(d => d.Store).WithMany(p => p.Staff).HasConstraintName("FK__staffs__store_id__1CF15040");
+        });
+
+        modelBuilder.Entity<Stock>(entity =>
+        {
+            entity.HasKey(e => new { e.StoreId, e.ProductId }).HasName("PK__stocks__E68284D3015BEF0A");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.Stocks).HasConstraintName("FK__stocks__product___2B3F6F97");
+
+            entity.HasOne(d => d.Store).WithMany(p => p.Stocks).HasConstraintName("FK__stocks__store_id__2A4B4B5E");
+        });
+
+        modelBuilder.Entity<Store>(entity =>
+        {
+            entity.HasKey(e => e.StoreId).HasName("PK__stores__A2F2A30C006A7729");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,235 @@
+﻿using ConsoleApp13.Data;
+using ConsoleApp13.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+
+            // 1- List all customers' first and last names along with their email addresses.
+           
+            var customers = _context.Customers
+                .Select(c => new
+                {
+                    c.FirstName,
+                    c.LastName,
+                    c.Email
+                });
+              
+
+            foreach (var item in customers)
+                Console.WriteLine($"{item.FirstName} , {item.LastName} , {item.Email}");
+
+
+            // 2- Retrieve all orders processed by a specific staff member (staff_id = 3).
+
+            var ordersStaff = _context.Orders
+                .Where(o => o.StaffId == 3);
+                
+
+            foreach (var item in ordersStaff)
+                Console.WriteLine($"{item.StaffId} , {item.OrderId}");
+
+
+            // 3- Get all products that belong to a category named "Mountain Bikes".
+
+            var BikeProducts = _context.Products
+                .Where(p => p.Category.CategoryName == "Mountain Bikes");
+                
+
+            foreach (var item in BikeProducts)
+                Console.WriteLine($"{item.ProductId} , {item.ProductName}");
+
+
+            // 4- Count the total number of orders per store.
+
+            var orderCounts = _context.Orders
+                .GroupBy(o => o.StoreId)
+                .Select(g => new
+                {
+                    StoreId = g.Key,
+                    Count = g.Count()
+                });
+               
+
+            foreach (var item in orderCounts)
+                Console.WriteLine($"{item.StoreId} , {item.Count}");
+
+
+            // 5- List all orders that have not been shipped yet (shipped_date is null).
+            var pendOrders = _context.Orders
+                .Where(o => o.ShippedDate == null);
+                
+
+            foreach (var item in pendOrders)
+                Console.WriteLine($"{item.OrderId} , {item.CustomerId}");
+
+
+            // 6- Display each customer's full name and the number of orders they have placed.
+
+            var customerOrders = _context.Customers
+                .Select(c => new
+                {
+                    FullName = c.FirstName + " " + c.LastName,
+                    OrderCount = c.Orders.Count()
+                });
+               
+
+            foreach (var item in customerOrders)
+                Console.WriteLine($"{item.FullName} : {item.OrderCount}");
+
+
+            // 7- List all products that have never been ordered (not found in order_items).
+
+            var NOrderProducts = _context.Products
+                .Where(p => !p.OrderItems.Any());
+            
+
+            foreach (var item in NOrderProducts)
+                Console.WriteLine($"{item.ProductId} , {item.ProductName}");
+
+
+            // 8- Display products that have a quantity of less than 5 in any store stock.
+
+            var lowProducts = _context.Products
+                .Where(p => p.Stocks.Any(s => s.Quantity < 5));
+                
+
+            foreach (var item in lowProducts)
+                Console.WriteLine($"{item.ProductId} : {item.ProductName}");
+
+
+            // 9- Retrieve the first product from the products table.
+          
+            var firstProduct = _context.Products.FirstOrDefault();
+            if (firstProduct != null)
+                Console.WriteLine($"{firstProduct.ProductId} , {firstProduct.ProductName}");
+
+
+            // 10- Retrieve all products from the products table with a certain model year.
+
+            var productsYear = _context.Products
+                .Where(p => p.ModelYear == 2018);
+                
+
+            foreach (var item in productsYear)
+                Console.WriteLine($"{item.ProductId} , {item.ProductName} , {item.ModelYear}");
+
+
+            // 11- Display each product with the number of times it was ordered.
+            var productOrderCount = _context.Products
+                .Select(p => new
+                {
+                    ProductName = p.ProductName,
+                    OrderCount = p.OrderItems.Count()
+                });
+              
+
+            foreach (var item in productOrderCount)
+                Console.WriteLine($"{item.ProductName} , {item.OrderCount}");
+
+
+            // 12- Count the number of products in a specific category.
+         
+            int categoryProductCount = _context.Products
+                .Where(p => p.CategoryId == 1)
+                .Count();
+
+            Console.WriteLine(categoryProductCount);
+
+
+            // 13- Calculate the average list price of products.
+          
+            var averagePrice = _context.Products.Average(p => p.ListPrice);
+            Console.WriteLine(averagePrice);
+
+
+            // 14- Retrieve a specific product from the products table by ID.
+          
+            var productId = _context.Products.Find(5);
+            if (productId != null)
+                Console.WriteLine($"{productId.ProductId} , {productId.ProductName}");
+
+
+            // 15- List all products that were ordered with a quantity greater than 3 in any order.
+
+            var highQuantity = _context.OrderItems
+                .Where(oi => oi.Quantity > 3)
+                .Select(oi => oi.Product)
+                .Distinct();
+                
+
+            foreach (var item in highQuantity)
+                Console.WriteLine($"{item.ProductId} , {item.ProductName}");
+
+
+            // 16- Display each staff member's name and how many orders they processed.
+
+            var staffOrders = _context.Staffs
+                .Select(s => new
+                {
+                    StaffName = s.FirstName + " " + s.LastName,
+                    OrdersCount = s.Orders.Count()
+                });
+              
+
+            foreach (var item in staffOrders)
+                Console.WriteLine($"{item.StaffName} , {item.OrdersCount}");
+
+
+            // 17- List active staff members only (active = 1) along with their phone numbers.
+
+            var activeStaff = _context.Staffs
+                .Where(s => s.Active == 1);
+                
+
+            foreach (var item in activeStaff)
+                Console.WriteLine($"{item.FirstName} {item.LastName} , {item.Phone}");
+
+
+            // 18- List all products with their brand name and category name.
+
+            var productsWithBrandCategory = _context.Products
+                .Select(p => new
+                {
+                    ProductName = p.ProductName,
+                    BrandName = p.Brand.BrandName,
+                    CategoryName = p.Category.CategoryName
+                });
+                
+
+            foreach (var item in productsWithBrandCategory)
+                Console.WriteLine($"{item.ProductName} , {item.BrandName} , {item.CategoryName}");
+
+
+            // 19- Retrieve orders that are completed.
+
+            var completedOrders = _context.Orders
+                .Where(o => o.OrderStatus == 3);
+               
+            foreach (var item in completedOrders)
+                Console.WriteLine($"{item.OrderId} , {item.OrderStatus} , {item.OrderDate}");
+
+
+            // 20- List each product with the total quantity sold (sum of quantity from order_items).
+
+            var totalQuantity = _context.Products
+                .Select(p => new
+                {
+                    ProductName = p.ProductName,
+                    TotalQuantitySold = p.OrderItems.Sum(oi => oi.Quantity)
+                });
+                
+
+            foreach (var item in totalQuantity)
+                Console.WriteLine($"{item.ProductName} , {item.TotalQuantitySold}");
+
+
+            
+        }
+    }
+}
